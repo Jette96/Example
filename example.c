8036f1aabd5d38e9597dcbc55533242167eccd00
Jette96 <stuke@uni-hildesheim.de>
2024-07-07 15:34:20 +0200

    Auslagerung Definitionen in example.h

--- example.c
+++ example.c
@@ -1,5 +1,4 @@
-#define DEBUG
-#define printResult(x) printf("Das Ergebnis der Berechnung ist %i.", x)
+#include "example.h"
 
 int calculate (int number) {
     number = number * 2; 
---
+++ example.h
@@ -0,0 +1,9 @@
+#ifndef EXAMPLE_H_
+#define EXAMPLE_H_
+
+#define DEBUG
+#define printResult(x) printf("Das Ergebnis der Berechnung ist %i.", x)
+
+int calculate(int number);
+
+#endif
